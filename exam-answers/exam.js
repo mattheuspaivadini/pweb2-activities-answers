f0b277ebec709fb9b91f8a3e1b4cfaafba2345be
mattheuspaivadini <matheuspaiva609@gmail.com>
2026-03-30 22:11:58 -0300

    doc: example created

--- exam-answers/exam.js
+++ exam-answers/exam.js
@@ -5,11 +5,11 @@ class Exam {
     this.correctAnswer = correctAnswer;
     this.studentsAnswer = [];
   }
-
+  
   add(answers) {
     this.studentsAnswer.push(answers);
   }
-
+  
   avg(answers) {
     let score = 0;
     for (let i = 0; i < answers.length; i++) {
@@ -19,19 +19,19 @@ class Exam {
   }
     return score;
 } 
-
+  
   min(n) {
   const scores = this.studentsAnswer.map(answers => this.avg(answers));
   scores.sort((a, b) => a - b); 
   return scores.slice(0, n);    
   }
-
+  
   max(n) {
     const scores = this.studentsAnswer.map(answers => this.avg(answers));
     scores.sort((a, b) => b - a); 
     return scores.slice(0, n);    
   }
-
+  
   lt(num) {
     const result = [];
 
@@ -57,4 +57,22 @@ class Exam {
 }
 
 }
-    
+
+
+const example = new Exam([2, 2, 2, 2, 2], ['a', 'b', 'a', 'c', 'd']);
+
+example.add(['a', 'b', 'b', 'b', 'b']);
+example.add(['a', 'b', 'a', 'c', 'd']);
+example.add(['a', 'b', 'a', 'c', 'b']);
+example.add(['a', 'a', 'a', 'c', 'd']);
+example.add(['b', 'c', 'a', 'c', 'd']);
+
+console.log(example.avg(example.studentsAnswer[0]));
+console.log(example.avg(example.studentsAnswer[1]));
+console.log(example.avg(example.studentsAnswer[2]));
+console.log(example.avg(example.studentsAnswer[3]));
+console.log(example.avg(example.studentsAnswer[4]));
+console.log(example.min(3));
+console.log(example.max(4));
+console.log(example.lt(5));
+console.log(example.gt(3)); 
