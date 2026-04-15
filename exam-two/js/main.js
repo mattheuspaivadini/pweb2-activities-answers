d9802bf06d920579f912f9d31d7a1ca3fe0bfe67
mattheuspaivadini <matheuspaiva609@gmail.com>
2026-04-15 00:22:40 -0300

    fix: responsive issues w/ small devices

--- exam-two/js/main.js
+++ exam-two/js/main.js
@@ -4,11 +4,11 @@ const mainContainer = document.querySelector('main.row');
 function renderFlags() {
     flags.forEach(flag => {
         const flagElement = document.createElement('div');
-        flagElement.classList.add('col-3', 'mb-4', 'col-lg-2', 'mb-4');
+        flagElement.classList.add('col-12', 'col-sm-6', 'col-md-4', 'col-lg-3', 'mb-4');
         flagElement.innerHTML = `
             <div class="card">
-                <img src="${flag.image}" class="card-img-top border-bottom" alt="${flag.name}" style="height: 100px; object-fit: contain;">
-                <div class="card-body">
+                <img src="${flag.image}" class="card-img-top border-bottom" alt="${flag.name}" style="height: 150px; object-fit: cover; background-color: #f8f9fa;">
+                <div class="card-body d-flex align-items-center justify-content-center">
                     <h5 class="card-title">${flag.name}</h5>
                 </div>
             </div>
